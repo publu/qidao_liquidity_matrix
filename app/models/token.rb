f5213dfbed8c932da3a550ae726d2102637dee03
spicy-muaddib <geekvineqidao@gmail.com>
2022-07-28 21:24:58 -0400

    Added scope to sort by risk grading

--- app/models/token.rb
+++ app/models/token.rb
@@ -2,4 +2,15 @@ class Token < ApplicationRecord
   validates :network_id, presence: true
   belongs_to :network
 
+  GRADES_ORDERED = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-"]
+  scope :order_by_grade, -> { order(Arel.sql(order_by_case)) }
+
+  def self.order_by_case
+    ret = "CASE"
+    GRADES_ORDERED.each_with_index do |p, i|
+      ret << " WHEN grade = '#{p}' THEN #{i}"
+    end
+      ret << " END"
+  end
+
 end
