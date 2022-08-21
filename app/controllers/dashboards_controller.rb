945076d6b6b432228ec858be4669c8ff8baa8242
spicy-muaddib <geekvineqidao@gmail.com>
2022-08-21 15:51:03 -0400

    Do not display unreleased vaults tokens in dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,11 @@ require 'json'
 class DashboardsController < ApplicationController
 
   def index
-    @chart_liquidity = Token.group(:symbol).sum(:liquidity)
-    @chart_volume = Token.group(:symbol).sum(:risk_volume)
-    @byliquidity = Token.order(liquidity: :desc).limit(10)
-    @byvolatility = Token.order(risk_volatility: :asc).limit(10)
-    @byscore = Token.order_by_grade.order(liquidity: :desc).limit(10)
+    @chart_liquidity = Token.where.not(minter_id: 4).group(:symbol).sum(:liquidity)
+    @chart_volume = Token.where.not(minter_id: 4).group(:symbol).sum(:risk_volume)
+    @byliquidity = Token.where.not(minter_id: 4).order(liquidity: :desc).limit(10)
+    @byvolatility = Token.where.not(minter_id: 4).order(risk_volatility: :asc).limit(10)
+    @byscore = Token.where.not(minter_id: 4).order_by_grade.order(liquidity: :desc).limit(10)
   end
 
 end
