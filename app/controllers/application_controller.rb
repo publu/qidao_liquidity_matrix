f53d24ad16d86a798be3d245332480540d24c3d5
spicy-muaddib <geekvineqidao@gmail.com>
2022-08-21 13:43:17 -0400

    Implemented http header language detection and added Spanish translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
+  end
+
+  def locale_from_header
+    parsed_locale = request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ?
+      parsed_locale.to_sym :
+      nil
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module QidaoLiquidity
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :es, :de]
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
   end
 end
