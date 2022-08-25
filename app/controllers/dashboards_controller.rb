0f763dd0a5d8871cf4d0693585e212cf8ed9840e
spicy-muaddib <geekvineqidao@gmail.com>
2022-08-25 01:45:44 -0400

    Updated dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,11 @@ require 'json'
 class DashboardsController < ApplicationController
 
   def index
+    @liquidity = Token.all.sum(:liquidity)
+    @networks = Network.all
+    @tokens = Token.all
+    @minters = Minter.where.not(link: "")
+    @mai_debt = Token.sum(:mai_debt)
     @chart_liquidity = Token.where.not(minter_id: 4).group(:symbol).sum(:liquidity)
     @chart_volume = Token.where.not(minter_id: 4).group(:symbol).sum(:risk_volume)
     @byliquidity = Token.includes(:network).where.not(minter_id: 4).order(liquidity: :desc).limit(10)
