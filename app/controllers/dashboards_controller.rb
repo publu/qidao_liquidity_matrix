6267c12f52712490897c6ea060b102781753b91d
spicy-muaddib <geekvineqidao@gmail.com>
2022-11-02 17:06:37 -0400

    Backend updates

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,7 +7,7 @@ class DashboardsController < ApplicationController
     @tokens = Token.all
     @minters = Minter.where.not(link: "")
     @chart_debt = Network.group(:name).sum(:liquidity)
-    @chart_liquidity = Token.where.not(minter_id: 4).group(:symbol).order(liquidity: :desc).limit(25).sum(:liquidity)
+    @chart_liquidity = Token.where.not(minter_id: 4).group(:symbol).sum(:liquidity)
     @byliquidity = Token.includes(:network).where.not(minter_id: 4).order(liquidity: :desc).limit(25)
     @byvolatility = Token.includes(:network).where.not(minter_id: 4).order(risk_volatility: :asc).limit(20)
     @byscore = Token.includes(:network).where.not(minter_id: 4).order_by_grade.order(liquidity: :desc).limit(20)
