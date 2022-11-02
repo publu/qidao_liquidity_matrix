77ecb241592ed37f2542bebf3ebddac3b54ef423
spicy-muaddib <geekvineqidao@gmail.com>
2022-11-01 22:48:32 -0400

    Backend updates

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,11 +9,10 @@ class DashboardsController < ApplicationController
     @mai_debt = Token.sum(:mai_debt)
     @chart_debt = Network.all.pluck(:name, :debtpercent)
     @chain_debt = Network.all
-    @chart_liquidity = Token.where.not(minter_id: 4).group(:symbol).sum(:liquidity)
-    @byliquidity = Token.includes(:network).where.not(minter_id: 4).order(liquidity: :desc).limit(25)
-    @byvolatility = Token.includes(:network).where.not(minter_id: 4).order(risk_volatility: :asc).limit(25)
-    @byscore = Token.includes(:network).where.not(minter_id: 4).order_by_grade.order(liquidity: :desc).limit(25)
-    @bydebt = Token.includes(:network).where(minter_id: 1).order(mai_debt: :desc).limit(20)
+    @chart_liquidity = Token.where.not(minter_id: 4).group(:symbol).order(liquidity: :desc).limit(15).sum(:liquidity)
+    @byliquidity = Token.includes(:network).where.not(minter_id: 4).order(liquidity: :desc).limit(20)
+    @byvolatility = Token.includes(:network).where.not(minter_id: 4).order(risk_volatility: :asc).limit(20)
+    @byscore = Token.includes(:network).where.not(minter_id: 4).order_by_grade.order(liquidity: :desc).limit(20)
   end
 
 end
