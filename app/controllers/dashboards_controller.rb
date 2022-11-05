de76add343777d8b96e103218aa782368b289463
spicy-muaddib <geekvineqidao@gmail.com>
2022-11-05 11:05:03 -0400

    Added MAI backing by risk grading chart to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,7 +9,7 @@ class DashboardsController < ApplicationController
     @chain_debt = Network.group(:name).sum(:debtamount)
     @collateral_debt = Token.where.not(minter_id: 4).group(:symbol).order(:mai_debt).sum(:mai_debt)
     @backing_type = Token.where.not(minter_id: 4).group(:token_type).order(:mai_debt).sum(:mai_debt)
-    @liquidity_debt = Token.where.not(minter_id: 4).group(:symbol).order(:liquidity).sum(:mai_debt)
+    @grade_debt = Token.where.not(minter_id: 4).group(:grade).order(:grade).sum(:mai_debt)
     @byliquidity = Token.includes(:network).where.not(minter_id: 4).order(liquidity: :desc).limit(20)
     @byvolatility = Token.includes(:network).where.not(minter_id: 4).order(risk_volatility: :asc).limit(20)
     @byscore = Token.includes(:network).where.not(minter_id: 4).order_by_grade.order(liquidity: :desc).limit(20)
