12d01e60dbd3ce69e0952335f49628b4e2da0121
spicy-muaddib <geekvineqidao@gmail.com>
2022-11-22 07:15:43 -0500

    Regression

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,10 +6,10 @@ class DashboardsController < ApplicationController
     @networks = Network.all
     @tokens = Token.all
     @minters = Minter.where.not(link: "")
-    @chain_debt = Network.all.group(:name).sum(:debtamount)
-    @collateral_debt = Token.where.not(minter_id: 4).order(mai_debt: :desc).group(:symbol).sum(:mai_debt)
+    @chain_debt = Network.group(:name).sum(:debtamount)
+    @collateral_debt = Token.where.not(minter_id: 4).group(:symbol).sum(:mai_debt)
     @backing_type = Token.where.not(minter_id: 4).group(:token_type).sum(:mai_debt)
-    @grade_debt = Token.where.not(minter_id: 4).order_by_grade.group(:grade).sum(:mai_debt)
+    @grade_debt = Token.where.not(minter_id: 4).group(:grade).sum(:mai_debt)
     @byliquidity = Token.includes(:network).where.not(minter_id: 4).order(liquidity: :desc).limit(20)
     @byvolatility = Token.includes(:network).where.not(minter_id: 4).order(risk_volatility: :asc).limit(20)
     @byscore = Token.includes(:network).where.not(minter_id: 4).order_by_grade.order(liquidity: :desc).limit(20)
