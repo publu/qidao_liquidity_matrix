e2a736d122499e6f5550128ca3f171229a036a2c
spicy-muaddib <geekvineqidao@gmail.com>
2023-02-09 00:34:28 -0500

    UI updates

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -14,11 +14,11 @@ class DashboardsController < ApplicationController
     @collateral_debt = Token.joins(:network).where.not(minter_id: 4).group(:symbol).sum(:mai_debt)
     @backing_type = Token.where.not(minter_id: 4).group(:token_type).sum(:mai_debt)
     @grade_debt = Token.where.not(minter_id: 4).group(:grade).sum(:mai_debt)
-    @mai_volatility = stable_prices.where(stables: { symbol: "MAI" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), p.volatility] }
-    @lusd_volatility = stable_prices.where(stables: { symbol: "LUSD" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), p.volatility] }
-    @mim_volatility = stable_prices.where(stables: { symbol: "MIM" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), p.volatility] }
-    @susd_volatility = stable_prices.where(stables: { symbol: "sUSD" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), p.volatility] }
-    @alusd_volatility = stable_prices.where(stables: { symbol: "alUSD" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), p.volatility] }
+    @mai_volatility = stable_prices.where(stables: { symbol: "MAI" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), (p.volatility * 100)] }
+    @lusd_volatility = stable_prices.where(stables: { symbol: "LUSD" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), (p.volatility * 100)] }
+    @mim_volatility = stable_prices.where(stables: { symbol: "MIM" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), (p.volatility * 100)] }
+    @susd_volatility = stable_prices.where(stables: { symbol: "sUSD" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), (p.volatility * 100)] }
+    @alusd_volatility = stable_prices.where(stables: { symbol: "alUSD" }).last(90).map { |p| [p.price_date.strftime("%b %d, %Y"), (p.volatility * 100)] }
   end
 
   def stables
